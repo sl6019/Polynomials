b105b61786189af5c24c6836ea3857fdbce9ae36
Sizhe Li <sizhe.li19@imperial.ac.uk>
2021-08-07 14:45:02 +0800

    A first method

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,3 +1,6 @@
 class Polynomial:
     def __init__(self, coefs):     #It has to be self for the first.
         self.coefficients = coefs
+
+    def degree(self):
+        return len(self.coefficients) - 1

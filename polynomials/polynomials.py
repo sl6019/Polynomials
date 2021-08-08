522dc684a8354ab59bc414ba141be265d6a02bee
Sizhe Li <sizhe.li19@imperial.ac.uk>
2021-08-08 14:45:58 +0800

    correlation

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -38,3 +38,48 @@ class Polynomial:
             return NotImplemented
     def __radd__(self, other):
         return self + other
+
+###############################################################
+# def __sub__(self, other):
+#         if isinstance(other, Polynomial):
+#             common = min(self.degree(), other.degree()) + 1
+#             coefs = tuple(a - b for a, b in zip(self.coefficients,
+#                                                 other.coefficients))
+#             coefs -= (self.coefficients[common:] + other.coefficients[common:])
+
+#             return Polynomial(coefs)
+
+#         elif isinstance(other, Number):
+#             return Polynomial((self.coefficients[0] - other,)
+#                               + self.coefficients[1:])
+
+#         else:
+#             return NotImplemented
+
+#     def __rsub__(self, other):
+#         return other - self
+
+#     def __mul__(self, other):
+#         coefs1 = self.coefficients
+#         coefs2 = other.coefficients
+
+#         list1 = [0 for i in range(self.degree() + other.degree() + 1)]
+#         if isinstance(other, Number):
+#             list1 = [other * i for i in self.coefficients]
+#             tuple1 = tuple(list1)
+#             return Polynomial(tuple1)
+    
+#         elif isinstance(other, Polynomial):
+#             for i in range(self.degree() + other.degree() + 1):
+#                 sum = 0
+#                 for j in range(self.degree()+1):
+#                     sum += coefs1[j] * coefs2[i - j]
+#                 list1[i] = sum
+#             return Polynomial(tuple(list1))
+
+#         else:
+#             return NotImplemented
+
+
+#     def __rmul__(self, other):
+#         return self * other
